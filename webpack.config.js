001a4d50a43a72f4ce54c7f445c7e64cdd3cb0d6
yujin mac <kimyu28jin@gmail.com>
2021-03-04 16:37:57 +0900

    style-loader, css-loader 적용

--- webpack.config.js
+++ webpack.config.js
@@ -9,4 +9,12 @@ module.exports = {
     path: path.resolve("./dist"),
     filename: "[name].js",
   },
+  module: {
+    rules: [
+      {
+        test: /\.css$/,
+        use: ["style-loader", "css-loader"],
+      },
+    ],
+  },
 };
